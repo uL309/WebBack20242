f9d3c7d9664fa51bb7f32357d74e7cd704d686c1
Lucas Camargo Vianna <camargo.vianna@pucpr.edu.br>
2024-09-17 18:48:45 -0300

    update added hub

--- builder/src/main/java/com/example/builder/BuilderDTO.java
+++ builder/src/main/java/com/example/builder/BuilderDTO.java
@@ -1,12 +1,21 @@
 package com.example.builder;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
 public class BuilderDTO {
+    @NotNull(message = "O faturamento não pode ser nulo.")
     private int id;
+    @NotNull(message = "O faturamento não pode ser nulo.")
     private String name;
+    @NotNull(message = "O faturamento não pode ser nulo.")
     private String role;
+    @NotNull(message = "O faturamento não pode ser nulo.")
+    @DecimalMin(value = "0.01", inclusive = true, message = "O faturamento deve ser maior que zero.")
     private float faturamento;
     private boolean icms;
     private boolean iss;
+    @NotNull(message = "O faturamento não pode ser nulo.")
     private int anexo;
     private boolean cofins;
     private boolean csll;
