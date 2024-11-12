0e6716395646c938f222858eb670a4568e872450
Lucas Camargo Vianna <camargo.vianna@pucpr.edu.br>
2024-11-12 10:33:07 -0300

    new JwtRequestFilter

--- hub/src/main/java/com/example/hub/JWT/JwtRequestFilter.java
+++ hub/src/main/java/com/example/hub/JWT/JwtRequestFilter.java
@@ -1,52 +1,68 @@
 package com.example.hub.JWT;
 
-import java.io.IOException;
-import java.security.Key;
-import java.util.Base64;
-
-import javax.crypto.spec.SecretKeySpec;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletResponseWrapper;
 
-@Order(2)
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
 public class JwtRequestFilter extends OncePerRequestFilter {
-    private String JWT_SECRET = "OKxypd6JJLjq4kIc3WvoPT1JYGfE1dPjv4fMNvi77reo7HxnnKFbr83k3CaXyzbHzPZnelJTyDt/PujEO+st1g==";
 
+    @Autowired
+    private UserDetailsService userDetailsService;
 
+    private final String SECRET_KEY = "OKxypd6JJLjq4kIc3WvoPT1JYGfE1dPjv4fMNvi77reo7HxnnKFbr83k3CaXyzbHzPZnelJTyDt/PujEO+st1g==";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException {
-        if (request.getRequestURI().startsWith("/eureka")) {
-            chain.doFilter(request, response);
-            return;
-        }
-
         final String authorizationHeader = request.getHeader("Authorization");
 
+        String username = null;
         String jwt = null;
+
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
             try {
-                jwt = authorizationHeader.substring(7);
-                byte[] secretBytes = Base64.getDecoder().decode(JWT_SECRET);
-                Key key = new SecretKeySpec(secretBytes, 0, secretBytes.length, "HmacSHA512");
-                Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
-                request.setAttribute("claims", claims);
-            } catch (io.jsonwebtoken.security.SecurityException e) {
-                response.sendError(HttpServletResponseWrapper.SC_UNAUTHORIZED, "Token inválido");
-                return;
+                Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(jwt).getBody();
+                username = claims.getSubject();
+            } catch (SignatureException e) {
+                logger.warn("Token JWT inválido");
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (validateToken(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
 
         chain.doFilter(request, response);
     }
+
+    private boolean validateToken(String token, UserDetails userDetails) {
+        final String username = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+        return (username.equals(userDetails.getUsername()));
+    }
 }
+
+
