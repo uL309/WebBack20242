3527b9f5713e13002ed64873dbc3d2490ca8aaf7
Guilherme Liudi Ioshii <105391453+guiioshii@users.noreply.github.com>
2024-11-12 10:15:22 -0300

    atualizacao jwt

--- hub/src/main/java/com/example/hub/JWT/JwtRequestFilter.java
+++ hub/src/main/java/com/example/hub/JWT/JwtRequestFilter.java
@@ -1,28 +1,27 @@
 package com.example.hub.JWT;
 
+import java.io.IOException;
+import java.security.Key;
+import java.util.Base64;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpServletResponseWrapper;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.security.Key;
-import javax.crypto.spec.SecretKeySpec;
-import java.util.Base64;
-
 @Order(2)
 public class JwtRequestFilter extends OncePerRequestFilter {
+    private String JWT_SECRET = "OKxypd6JJLjq4kIc3WvoPT1JYGfE1dPjv4fMNvi77reo7HxnnKFbr83k3CaXyzbHzPZnelJTyDt/PujEO+st1g==";
 
-    @Value("${jwt.secret}")
-    private String JWT_SECRET;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
@@ -39,7 +38,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             try {
                 jwt = authorizationHeader.substring(7);
                 byte[] secretBytes = Base64.getDecoder().decode(JWT_SECRET);
-                Key key = new SecretKeySpec(secretBytes, 0, secretBytes.length, "HmacSHA256");
+                Key key = new SecretKeySpec(secretBytes, 0, secretBytes.length, "HmacSHA512");
                 Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
                 request.setAttribute("claims", claims);
             } catch (io.jsonwebtoken.security.SecurityException e) {
